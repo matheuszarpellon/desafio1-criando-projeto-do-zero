941bda50dfa7271cd68df14eae5a1994eba963bc
matheuszarpellon <mvzarpellon@gmail.com>
2021-06-09 06:06:54 -0300

    desafio 2 finalizado

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,6 +13,7 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script async defer src="https://static.cdn.prismic.io/prismic.js?new=true&repo=mvz-blog"></script>
         </body>
       </Html>
     )
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -11,9 +11,12 @@ import { getPrismicClient } from '../../services/prismic';
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 import { ptBR } from 'date-fns/locale';
+import Link from 'next/link';
+import Comments from '../../components/Comments';
 
 interface Post {
   first_publication_date: string | null;
+  last_publication_date: string | null;
   data: {
     title: string;
     banner: {
@@ -30,10 +33,25 @@ interface Post {
 }
 
 interface PostProps {
-  post: Post;
+  post: Post
+  navigation: {
+    prevPost: {
+      uid: string,
+      data: {
+        title: string
+      }
+    }[]
+    nextPost: {
+      uid: string,
+      data: {
+        title: string
+      }
+    }[]
+  }
+  preview: boolean
 }
 
-export default function Post({post}: PostProps): JSX.Element {
+export default function Post({ post, navigation, preview }: PostProps): JSX.Element {
   const totalWords = post.data.content.reduce((total, contentItem) => {
     total += contentItem.heading.split(' ').length
 
@@ -52,11 +70,25 @@ export default function Post({post}: PostProps): JSX.Element {
 
   const formatedDate = format(
     new Date(post.first_publication_date),
-        'dd MMM yyyy',
-        {
-          locale: ptBR,
-        }
+    'dd MMM yyyy',
+    {
+      locale: ptBR,
+    }
   )
+
+  const isPostEdited =
+    post.first_publication_date !== post.last_publication_date
+
+  let editionDate
+  if (isPostEdited) {
+    editionDate = format(
+      new Date(post.last_publication_date),
+      "'* editado em' dd MMM yyyy' , às' H':'m",
+      {
+        locale: ptBR,
+      }
+    )
+  }
   return (
     <>
       <Header />
@@ -66,32 +98,62 @@ export default function Post({post}: PostProps): JSX.Element {
           <div className={styles.postTop}>
             <h1>{post.data.title}</h1>
             <ul>
-                  <li>
-                    <FiCalendar />
-                    {formatedDate}
-                  </li>
-                  <li>
-                    <FiUser />
-                    {post.data.author}
-                  </li>
-                  <li>
-                    <FiClock />{`${readTime} min`} 
-                  </li>
-                </ul>
+              <li>
+                <FiCalendar />
+                {formatedDate}
+              </li>
+              <li>
+                <FiUser />
+                {post.data.author}
+              </li>
+              <li>
+                <FiClock />{`${readTime} min`}
+              </li>
+            </ul>
+            {isPostEdited && <span>{editionDate}</span> }
           </div>
 
-          {post.data.content.map(content =>{
+          {post.data.content.map(content => {
             return (
               <article key={content.heading}>
-            <h2>{content.heading}</h2>
-            <div className={styles.postContent}
-            dangerouslySetInnerHTML={{ __html:RichText.asHtml(content.body)}} />
-          </article>
+                <h2>{content.heading}</h2>
+                <div className={styles.postContent}
+                  dangerouslySetInnerHTML={{ __html: RichText.asHtml(content.body) }} />
+              </article>
 
             )
           })}
-          
+
         </div>
+
+        <section className={`${styles.navigation} ${styles.container}`}>
+          {navigation?.prevPost.length > 0 && (
+            <div>
+              <h3>{navigation.prevPost[0].data.title}</h3>
+              <Link href={`/post/${navigation.prevPost[0].uid}`}>
+                <a>Post anterior</a>
+              </Link>
+            </div>
+          )}
+
+          {navigation?.nextPost.length > 0 && (
+            <div>
+              <h3>{navigation.nextPost[0].data.title}</h3>
+              <Link href={`/post/${navigation.nextPost[0].uid}`}>
+                <a>Próximo post</a>
+              </Link>
+            </div>
+          )}
+        </section>
+        <Comments />
+
+        {preview && (
+          <aside>
+            <Link href="/api/exit-preview">
+              <a className={commonStyles.preview}>Sair do modo preview</a>
+            </Link>
+          </aside>
+        )}
       </main>
     </>
   )
@@ -115,18 +177,37 @@ export const getStaticPaths = async () => {
     paths,
     fallback: true,
   }
-
-  // TODO
 };
 
-export const getStaticProps: GetStaticProps = async context => {
+export const getStaticProps: GetStaticProps = async ({ params, preview = false, previewData }) => {
   const prismic = getPrismicClient();
-  const {slug} = context.params;
-  const response = await prismic.getByUID('posts', String(slug), {});
+  const { slug } = params;
+  const response = await prismic.getByUID('posts', String(slug), {
+    ref: previewData?.ref || null,
+  });
+
+  const prevPost = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      pageSize: 1,
+      after: response.id,
+      orderings: '[document.first_publication_date]'
+    }
+  )
+
+  const nextPost = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      pageSize: 1,
+      after: response.id,
+      orderings: '[document.last_publication_date]'
+    }
+  )
 
   const post = {
     uid: response.uid,
     first_publication_date: response.first_publication_date,
+    last_publication_date: response.last_publication_date,
     data: {
       title: response.data.title,
       subtitle: response.data.subtitle,
@@ -146,6 +227,11 @@ export const getStaticProps: GetStaticProps = async context => {
   return {
     props: {
       post,
+      navigation: {
+        prevPost: prevPost?.results,
+        nextPost: nextPost?.results,
+      },
+      preview,
     }
   }
 };
